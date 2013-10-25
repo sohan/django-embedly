a3fa29e23c6399adc8e40aab1edd59a931017bd2
Sohan Jain <sj346@cornell.edu>
2013-10-25 13:37:27 -0400

    Let templatetag take in a context variable, not just a url

--- embeds/templatetags/embed_filters.py
+++ embeds/templatetags/embed_filters.py
@@ -1,3 +1,4 @@
+import re
 from django import template
 from django.conf import settings
 
@@ -6,13 +7,50 @@ from embeds.models import SavedEmbed
 
 register = template.Library()
 
+#url regex found here: http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+URL_REGEX = re.compile(r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""")
+
 USER_AGENT = 'Mozilla/5.0 (compatible; django-embedly/0.2; ' \
         '+http://github.com/BayCitizen/)'
 
+def _get_url_from_context_variable(var):
+    '''
+    @var: string, context variable
+    Return the first instance of a url in var
+    '''
+    match = URL_REGEX.search(var)
+    if match:
+        return match.group(0)
+    return None
+
 @register.assignment_tag
-def get_oembed_data(url, maxwidth=None):
+def get_oembed_data(context_var, maxwidth=None):
     '''
+    A custom templatetag that returns an object representing 
+        all oembed data for a given url found in a context variable
+    Usage:
+    {% load embed_filters %}
+    {% with context_var="Check out my cool photo: http://www.flickr.com/photos/visualpanic/233508614/" %}
+        {{ context_var }}
+
+        {% get_oembed_data context_var maxwidth=400 as oembed %}
+        <div class="embed-data">
+            {{oembed.title}} <br />
+            {{oembed.description}} <br />
+            {{oembed.html}} <br />
+        </div>
+
+    {% endwith %}
+
+    Uses the Embedly API to get oembed data. Always look to the db/cache first,
+        and then fall back to the Embedly API for speed 
+        (at the cost of accuracy & updated urls).
     '''
+
+    url = _get_url_from_context_variable(context_var)
+    if not url:
+        return {}
+
     try:
         saved_embed = SavedEmbed.objects.get(url=url, maxwidth=maxwidth)
     except SavedEmbed.DoesNotExist:
