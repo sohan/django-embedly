b50379fdbd3f606af05386df4b02936297b60ce6
Sohan Jain <sj346@cornell.edu>
2013-10-28 16:08:14 -0400

    add django-cache-machine

--- embeds/models.py
+++ embeds/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from jsonfield import JSONField
+import caching.base
 
 OEMBED_TYPES = (
     ('video',)*2,
@@ -8,13 +9,15 @@ OEMBED_TYPES = (
     ('rich',)*2,
 )
 
-class SavedEmbed(models.Model):
+class SavedEmbed(caching.base.CachingMixin, models.Model):
     url = models.URLField()
     maxwidth = models.SmallIntegerField(null=True, blank=True)
     maxheight = models.SmallIntegerField(null=True, blank=True)
     type = models.CharField(max_length=10, choices=OEMBED_TYPES)
     oembed_data = JSONField()
     last_updated = models.DateTimeField(auto_now=True)
+
+    objects = caching.base.CachingManager()
 
     class Meta:
         unique_together = ('url', 'maxwidth', 'maxheight')
